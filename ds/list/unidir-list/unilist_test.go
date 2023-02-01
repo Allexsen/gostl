2d1a1e7ea22bfe18cae8a01c74baa759923b3461
Alex <asvanidze12@gmail.com>
2023-02-01 23:58:39 +0400

    Linked List - Test Updates

--- ds/list/unidir-list/unilist_test.go
+++ ds/list/unidir-list/unilist_test.go
@@ -16,19 +16,19 @@ func TestList(t *testing.T) {
 	list.PushFront(2)
 
 	assert.Equal(t, 2, list.Len())
-	assert.Equal(t, "[2 1]", list.String())
+	assert.Equal(t, "2->1->nil", list.String())
 
 	list.PushBack(3)
 	list.PushBack(4)
-	assert.Equal(t, "[2 1 3 4]", list.String())
+	assert.Equal(t, "2->1->3->4->nil", list.String())
 
 	list.MakeHead(list.Head(), list.Head().Next())
 
-	assert.Equal(t, "[1 2 3 4]", list.String())
+	assert.Equal(t, "1->2->3->4->nil", list.String())
 
 	list.MakeTail(list.Head(), list.Head().Next())
 
-	assert.Equal(t, "[1 3 4 2]", list.String())
+	assert.Equal(t, "1->3->4->2->nil", list.String())
 }
 
 func TestList_InsertAfter(t *testing.T) {
@@ -37,13 +37,13 @@ func TestList_InsertAfter(t *testing.T) {
 		list.PushBack(i)
 	}
 	list.InsertAfter(6, list.Head())
-	assert.Equal(t, "[1 6 2 3 4 5]", list.String())
+	assert.Equal(t, "1->6->2->3->4->5->nil", list.String())
 
 	list.InsertAfter(7, list.Head().Next())
-	assert.Equal(t, "[1 6 7 2 3 4 5]", list.String())
+	assert.Equal(t, "1->6->7->2->3->4->5->nil", list.String())
 
 	list.InsertAfter(8, list.Tail())
-	assert.Equal(t, "[1 6 7 2 3 4 5 8]", list.String())
+	assert.Equal(t, "1->6->7->2->3->4->5->8->nil", list.String())
 }
 
 func TestList_Remove(t *testing.T) {
@@ -52,10 +52,10 @@ func TestList_Remove(t *testing.T) {
 		list.PushBack(i)
 	}
 	list.Remove(nil, list.Head())
-	assert.Equal(t, "[2 3 4 5]", list.String())
+	assert.Equal(t, "2->3->4->5->nil", list.String())
 
 	list.Remove(list.Head(), list.Head().Next())
-	assert.Equal(t, "[2 4 5]", list.String())
+	assert.Equal(t, "2->4->5->nil", list.String())
 
 	list.PushFront(6)
 }
