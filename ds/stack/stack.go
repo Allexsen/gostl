3f22859f20a3139fbebb251d08c853f1788acab8
Alex <asvanidze12@gmail.com>
2023-02-03 13:22:11 +0400

    Added Comments

--- ds/stack/stack.go
+++ ds/stack/stack.go
@@ -15,7 +15,7 @@ type Stack[T any] struct {
 }
 
 func New[T any]() *Stack[T] {
-	return &Stack[T]{values: unilist.New[T]()}
+	return &Stack[T]{values: unilist.New[T]()} // Zero value declaration
 }
 
 func (stk *Stack[T]) Empty() bool {
@@ -26,30 +26,35 @@ func (stk *Stack[T]) Size() int {
 	return stk.values.Len()
 }
 
+// Returns the last element in the stack
 func (stk *Stack[T]) Top() T {
 	if stk.Empty() {
 		panic(ErrEmpty)
 	}
 
-	return stk.values.Head().Value
+	return stk.values.Head().Value // The last element is always the head of the list
 }
 
+// Pops the last element in the stack
 func (stk *Stack[T]) Pop() T {
 	if stk.Empty() {
 		panic(ErrEmpty)
 	}
 
-	return stk.values.PopFront().Value
+	return stk.values.PopFront().Value // The last element is always the head in the list
 }
 
+// Adds a new value on top of the stack
 func (stk *Stack[T]) Push(val T) {
-	stk.values.PushFront(val)
+	stk.values.PushFront(val) // New value will always be a new head
 }
 
+// Removes all elements from the stack
 func (stk *Stack[T]) Clear() {
 	stk.values.Clear()
 }
 
+// Stringifies the stack
 func (stk *Stack[T]) String() string {
 	var sb strings.Builder
 	sb.WriteString("[")
@@ -61,6 +66,9 @@ func (stk *Stack[T]) String() string {
 	}
 
 	sb.WriteString("]")
+
+	// Reverse the values, since stack is LIFO
+	// It just makes more sense to me personally
 	bytes := []byte(sb.String())
 	for l, r := 1, len(bytes)-2; l < r; l++ {
 		bytes[l], bytes[r] = bytes[r], bytes[l]
